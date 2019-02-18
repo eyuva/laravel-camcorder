16fb6c333f3c2076916cde0662e44fd66b4073db
Vishal Sancheti <mail@vishal.pro>
2019-02-18 16:29:36 +0530

    add images and generate video

--- src/Camcorder.php
+++ src/Camcorder.php
@@ -3,26 +3,62 @@
 namespace Eyuva\Camcorder;
 
 
+use Symfony\Component\Process\Process;
+
 class Camcorder
 {
     protected $fps;
+    protected $seq_file;
+    protected $seq_file_path;
+    protected $audio_file_path;
 
     public function __construct()
     {
         $this->fps = config('camcorder.fps',25);
+        $this->seq_file_path = storage_path('app/'.str_random(4).'.txt');
+        $this->seq_file = fopen($this->seq_file_path, 'w');
+        fwrite($this->seq_file, "ffconcat version 1.0\n");
+        fclose($this->seq_file);
+        $this->seq_file = fopen($this->seq_file_path, 'a');
     }
 
-    public function addImage($path, $duration = 1){
-        //
+    public function addImage($image_path, $duration = 1, $is_last = 0){
+        fwrite($this->seq_file, "file '".$image_path."'\n");
+        fwrite($this->seq_file, "duration ".$duration."\n");
+        if($is_last){
+            fwrite($this->seq_file, "file '".$image_path."'\n");
+            fwrite($this->seq_file, "duration 1 \n");
+        }
     }
 
-    public function addAudio($path)
+    public function addAudio($audio_path)
     {
-        //
+       //
     }
 
-    public function generate()
+    public function generate($output_path,$file_name)
     {
-        //
+        $cmd = 'ffmpeg';
+
+        //Add Images
+        if(filesize($this->seq_file_path)) {
+            $cmd .=' -f concat -safe 0 -i ' . $this->seq_file_path ;
+        }
+
+        //Video Filters
+        $cmd .= ' -vf format=yuv420p,fps=' . $this->fps;
+
+        //Output
+        $cmd .=  ' '.$output_path.'/'.$file_name.'.mp4  -y';
+
+        $process = new Process($cmd);
+        $process->run();
+
+        unlink($this->seq_file_path);
+        if (! $process->isSuccessful()) {
+            throw new \Exception(trim($process->getErrorOutput()));
+        }else{
+            fclose($this->seq_file);
+        }
     }
 }
