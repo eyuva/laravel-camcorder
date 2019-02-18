df1d61282bc5c418ae0dffdbd6e90e52e7d6a548
Vishal Sancheti <mail@vishal.pro>
2019-02-18 16:37:04 +0530

    config publish fix

--- src/CamcorderServiceProvider.php
+++ src/CamcorderServiceProvider.php
@@ -9,12 +9,12 @@ class CamcorderServiceProvider extends ServiceProvider
 {
     public function boot(){
         $this->publishes([
-            __DIR__.'/config/camcorder.php' => config_path('camcorder.php'),
+            __DIR__.'/../config/camcorder.php' => config_path('camcorder.php'),
         ]);
     }
 
     public function register(){
-        $this->mergeConfigFrom( __DIR__.'/../config/camcoder.php', 'camcorder');
+        $this->mergeConfigFrom( __DIR__.'/../config/camcorder.php', 'camcorder');
 
         $this->app->singleton('camcorder', function() {
             return new Camcorder();
